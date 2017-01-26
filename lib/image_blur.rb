e529d919569170122719ae33b86845f7fa8142c5
Brandon Elder <brandonelder3@gmail.com>
2017-01-26 00:35:49 +0000

    2 example pass

--- lib/image_blur.rb
+++ lib/image_blur.rb
@@ -11,30 +11,40 @@ class Image
     end
   end
 
+   def blur! (blur=1)
+    blur.times do
+      blur_place!
+    end
+    @image
+  end
+
   def blur_place!
     blur_place = []
     @image.each_with_index do |row, index|
       row.each_with_index do |x, row_index|
         blur_place << [index, row_index] if x == 1
-    end
-  end
-
-      blur_place.each do |place|
-        @image[place[0]][place[1] + 1] = 1 if place[1] + 1 <= @image[place[0]].length - 1
-        @image[place[0]][place[1] - 1] = 1 if place[1] - 1 >= 0
-        @image[place[0] + 1][place[1]] = 1 if place[0] + 1 <= @image.length - 1
-        @image[place[0] - 1][place[1]] = 1 if place[0] - 1 >= 0
       end
-
-      output_image
     end
+
+    blur_place.each do |place|
+      @image[place[0]][place[1] + 1] = 1 if place[1] + 1 <= @image[place[0]].length - 1
+      @image[place[0]][place[1] - 1] = 1 if place[1] - 1 >= 0
+      @image[place[0] + 1][place[1]] = 1 if place[0] + 1 <= @image.length - 1
+      @image[place[0] - 1][place[1]] = 1 if place[0] - 1 >= 0
+    end
+    output_image
+  end
 end
-   
-image2 = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-])
+ 
+
+#image2 = Image.new([
+  #[0, 0, 0, 0],
+  #[0, 1, 0, 0],
+  #[0, 0, 0, 1],
+  #[0, 0, 0, 0]
+#])
+
+#image.blur!(1)
+#image.output
 
 
--- spec/image_blur_spec.rb
+++ spec/image_blur_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Image, type: :model do 
   describe "image blur should work" do
-    it "should blur the image" do
+    it "should blur the image once" do
       input = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -23,6 +23,29 @@ RSpec.describe Image, type: :model do
 
       image = Image.new(input)
       expect(image.blur_place!).to eq expected 
+    end
+
+    it "should blur the image 2 times" do
+      input = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+
+      expected = [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0]
+      ]
+
+      image = Image.new(input)
+      expect(image.blur!(2)).to eq expected 
     end
   end
 end
